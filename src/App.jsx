5b6de36a7d2609d5028005631d45f2173812b591
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-05-01 09:13:58 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+          <h1 className="text-3xl font-bold underline">
+          Hello world!
+          <button className="btn">click</button>
+           
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
