90a2797507ac740a491d7ab3ef65391555f9262e
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-05-01 20:30:33 +0100

    data fetched from the api and mapped over the screen in a div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import UserResults from '../components/users/UserResults'
 
 
 
@@ -9,24 +10,25 @@ const Home = () => {
   // VITE_SOME_KEY=123
   // VITE_DB_PASSWORD=foobar
 
-  console.log(import.meta.env.VITE_SOME_KEY) // 123
-  console.log(import.meta.env.VITE_DB_PASSWORD) // undefined
+  // console.log(import.meta.env.VITE_SOME_KEY) // 123
+  // console.log(import.meta.env.VITE_DB_PASSWORD) // undefined
 
   // .env
   // VITE_GITHUB_URL = https://api.github.com
   // VITE_GITHUB_TOKEN=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
 
 
-  console.log(import.meta.env.VITE_GITHUB_URL) // 123
-  console.log(import.meta.env.VITE_GITHUB_TOKEN) // undefined
+  // console.log(import.meta.env.VITE_GITHUB_URL) // 123
+  // console.log(import.meta.env.VITE_GITHUB_TOKEN) // undefined
 
   
   return (
-    <div>
-      <h1 className='text-6xl'>Welcome</h1>
+    <>
       {/* {import.meta.env.VITE_GITHUB_TOKEN} */}
+      {/* search component */}
+      <UserResults />
       
-    </div>
+    </>
   )
 }
 
