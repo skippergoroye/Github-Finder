8a9add7a94989af34494a1be846d666c857927ff
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-05-01 12:42:08 +0100

    env added to project using import.meta.env.VITE_GITHUB_TOKEN

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
+
+
 const Home = () => {
+
+
+  // .env
+  // VITE_SOME_KEY=123
+  // VITE_DB_PASSWORD=foobar
+
+  console.log(import.meta.env.VITE_SOME_KEY) // 123
+  console.log(import.meta.env.VITE_DB_PASSWORD) // undefined
+
+  // .env
+  // VITE_GITHUB_URL = https://api.github.com
+  // VITE_GITHUB_TOKEN=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
+
+
+  console.log(import.meta.env.VITE_GITHUB_URL) // 123
+  console.log(import.meta.env.VITE_GITHUB_TOKEN) // undefined
+
+  
   return (
-    <div>Home</div>
+    <div>
+      <h1 className='text-6xl'>Welcome</h1>
+      {/* {import.meta.env.VITE_GITHUB_TOKEN} */}
+      
+    </div>
   )
 }
 
