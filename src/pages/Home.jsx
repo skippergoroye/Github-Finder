4676bf89e41351d756f48f5d7e3c5b00af4d660c
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-05-02 15:22:46 +0100

    new token added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,18 +14,12 @@ const Home = () => {
   // console.log(import.meta.env.VITE_SOME_KEY) // 123
   // console.log(import.meta.env.VITE_DB_PASSWORD) // undefined
 
-  // .env
-  // VITE_GITHUB_URL = https://api.github.com
-
-
-
-  // console.log(import.meta.env.VITE_GITHUB_URL) // 123
-  // console.log(import.meta.env.VITE_GITHUB_TOKEN) // undefined
+ 
 
   
   return (
     <>
-      {/* {import.meta.env.VITE_GITHUB_TOKEN} */}
+      {/* {import.meta.env.VITE_SOME_KEY} */}
       {/* search component */}
       <UserResults />
       
